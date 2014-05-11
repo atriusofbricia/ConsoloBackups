d52732668167e49a9516d3028dabdbc47f75e0c7
atriusofbricia <atriusofbricia@gmail.com>
2014-05-11 12:38:31 -0500

    Added date field, probably ready to deploy..

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140510152935) do
+ActiveRecord::Schema.define(version: 20140511162217) do
 
   create_table "backups", force: true do |t|
     t.string   "sourcehost"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140510152935) do
     t.string   "status"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.date     "date"
   end
 
   create_table "friendly_id_slugs", force: true do |t|
